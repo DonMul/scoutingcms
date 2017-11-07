eda0ec15f193e86fcf18f467f32623992afade7e
Joost Mul <joost@jmul.net>
2017-11-07 16:32:22 +0100

    Added dev setting for css compiling

--- public/css/compiled.php
+++ public/css/compiled.php
@@ -3,11 +3,23 @@
 require_once realpath(dirname(__FILE__) . '/../../private/bootstrap.php');
 
 $settings = \Lib\Core\Settings::getInstance();
-try {
-    $parser = new Less_Parser();
-    $parser->parseFile(__DIR__ . '/main.less', ($settings->get('ssl') ? 'https://' : 'http://') .$_SERVER['HTTP_HOST'] . '/css/');
-    header("Content-type: text/css");
-    echo $parser->getCss(); exit;
-} catch (\Exception $ex) {
-    var_dump($ex->getMessage());
+$compiledFile = dirname(__FILE__) . '/compiled.css';
+if ($settings->get('dev') !== true) {
+    if (file_exists($compiledFile)) {
+        echo file_get_contents($compiledFile);
+        exit;
+    }
 }
+
+$parser = new Less_Parser();
+$parser->parseFile(__DIR__ . '/main.less', ($settings->get('ssl') ? 'https://' : 'http://') .$_SERVER['HTTP_HOST'] . '/css/');
+header("Content-type: text/css");
+$css = $parser->getCss();
+
+if (file_exists($compiledFile)) {
+    unlink($compiledFile);
+}
+
+file_put_contents($compiledFile, $css);
+echo $css;
+exit;
