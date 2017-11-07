1c3b0b2035bd48550aae5bd5e212b0a2386515d3
Joost Mul <joost@jmul.net>
2017-11-07 12:39:34 +0100

    Make SSL Optional

--- private/index.php
+++ private/index.php
@@ -1,10 +1,13 @@
 <?php
 
-if (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] !== 'on') {
-    header('Location: https://scoutingflg.jmul.net');
-}
 session_start();
 require_once dirname(__FILE__) . '/bootstrap.php';
+
+if (\Lib\Core\Settings::getInstance()->get('ssl', false) == true &&
+    (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] !== 'on')
+) {
+    header('Location: https://' . $_SERVER['HTTP_HOST']);
+}
 
 try {
     $a = new \Lib\Core\Application();
