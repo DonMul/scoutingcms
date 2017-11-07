18eac9900bd824bc56ae793290ac877d5833ba18
Joost Mul <joost@jmul.net>
2017-11-07 11:54:22 +0100

    Add README.md + install script for MySQL

---
+++ README.md
@@ -0,0 +1,38 @@
+# ScoutingCMS
+## What is it?
+Scouting CMS is a CMS which is ment for Scouting Groups in order to manage their site.
+
+### What features are in ScoutingCMS?
+* Customizable pages
+* Photo albums
+* Information / page per subgroup
+* A custom admin panel
+  * Manage Credentials
+  * Manage Users
+  * Manage Pages
+  * Customize Menu
+
+## How to install
+I still want to make a simple install script which automates these steps, but for now:
+
+### Database
+Create a Database at your host and execute the `install/db.sql` file in order to create the setup.
+
+### Settings
+Create a settings file: `private/Conf/settings.yaml`. The contents of this file should be as follows:
+```
+database:
+  host: DATABASE_HOSTNAME
+  username: DATABASE_USERNAME
+  password: DATABASE_PASSWORD
+  database: DATABASE_NAME
+  prefix: OPTIONAL_DATABASE_TABLE_PREFIX
+```
+
+### Create your first user
+* Decide for a username and password
+* Hash the password with sha512 algorithm: https://passwordsgenerator.net/sha512-hash-generator/
+* Insert the user in MySQL: `INSERT INTO user (username, password, nickname, email) VALUES ('USERNAME', 'HASHED_PASSWORD', 'NAME OF OWNER', 'EMAIL ADDRESS');`
+
+## How to use
+Go to the `/admin` URL in order to start editing your site.
---
+++ install/db.sql
@@ -0,0 +1,211 @@
+CREATE TABLE `agenda` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `startDate` datetime NOT NULL,
+  `endDate` datetime DEFAULT NULL,
+  `description` text NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `category` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `agendaCategory` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `color` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `album` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `category` int(11) NOT NULL,
+  `thumbnail` varchar(255) NOT NULL,
+  `private` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `albumCategory` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `download` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `type` enum('report','newsletter') NOT NULL DEFAULT 'report',
+  `filename` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `group` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `picture` varchar(255) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE `menu` (
+  `id` int(11) NOT NULL,
+  `parentId` int(11) NOT NULL DEFAULT '0',
+  `name` varchar(255) NOT NULL,
+  `type` enum('page','album','calender','group','download','url') NOT NULL,
+  `value` varchar(255) NOT NULL,
+  `position` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `news` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `published` datetime NOT NULL,
+  `status` enum('draft','published') NOT NULL DEFAULT 'draft'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `page` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `header` varchar(255) NOT NULL,
+  `isHomepage` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `permission` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `picture` (
+  `id` int(11) NOT NULL,
+  `albumId` int(11) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `isAdmin` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `rolePermission` (
+  `id` int(11) NOT NULL,
+  `roleId` int(11) NOT NULL,
+  `permissionId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `nickname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE `userRole` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `roleId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ADD INDEXES
+
+ALTER TABLE `agenda`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `agendaCategory`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `album`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `albumCategory`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `download`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `group`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `page`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `permission`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `picture`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `albumId` (`albumId`);
+
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `rolePermission`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `settings`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+ALTER TABLE `userRole`
+  ADD PRIMARY KEY (`id`);
+
+-- ADD AUTO_INCREMENTS
+
+ALTER TABLE `agenda`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `agendaCategory`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `album`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `albumCategory`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `download`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `group`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `news`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `page`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `permission`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `picture`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `rolePermission`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `settings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `userRole`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
